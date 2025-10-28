420c2c994d712b57292b79ffd68e7995fe06183a
klow <202186742+7klow@users.noreply.github.com>
2025-10-28 10:31:49 +0100

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -78,3 +78,50 @@ int	ft_printf(const char *str, ...)
 	free(fstring);
 	return (ret);
 }
+
+/*
+int main(void)
+{
+    int				len1;
+    int				len2;
+    char			c;
+    char			*s;
+    void			*ptr;
+    int 			d;
+    unsigned int	u;
+    unsigned int	x;
+
+	c = 'A';
+	s = ft_strdup("Hello");
+	ptr = s;
+	d = -42;
+	u = 42;
+	x = 255;
+
+    len1 = ft_printf("Char: %c\n", c);
+    len1 += ft_printf("String: %s\n", s);
+    len1 += ft_printf("Pointer: %p\n", ptr);
+    len1 += ft_printf("Decimal: %d\n", d);
+    len1 += ft_printf("Integer: %i\n", d);
+    len1 += ft_printf("Unsigned: %u\n", u);
+    len1 += ft_printf("Hex lower: %x\n", x);
+    len1 += ft_printf("Hex upper: %X\n", x);
+    len1 += ft_printf("Percent: %%\n");
+    ft_printf("ft_printf total: %d\n\n", len1);
+
+    len2 = printf("Char: %c\n", c);
+    len2 += printf("String: %s\n", s);
+    len2 += printf("Pointer: %p\n", ptr);
+    len2 += printf("Decimal: %d\n", d);
+    len2 += printf("Integer: %i\n", d);
+    len2 += printf("Unsigned: %u\n", u);
+    len2 += printf("Hex lower: %x\n", x);
+    len2 += printf("Hex upper: %X\n", x);
+    len2 += printf("Percent: %%\n");
+    printf("printf total: %d\n\n", len2);
+
+	free(s);
+    return (0);
+}
+*/
+// cc -Wall -Wextra -Werror ft_printf.c utils.c utils_hex.c libft/*.c -o test
